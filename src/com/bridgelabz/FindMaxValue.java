af3ba3d187d122b8261722072eaa548463b8ce6f
Shubham <ssp671995@gmail.com>
2022-03-21 23:34:44 +0530

    Added welcome message

---
+++ src/com/bridgelabz/FindMaxValue.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class FindMaxValue {
+    public static void main(String[] args) {
+        System.out.println("welcome to Generics program");
+
+    }
+}
+
---
+++ src/com/bridgelabz/MaximumNumber.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class MaximumNumber {
+    //method: find maximum
+    public <T extends Comparable<T>> void findMaximum(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
+        } else if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        System.out.println("Maximun is: " + max);
+    }
+}
